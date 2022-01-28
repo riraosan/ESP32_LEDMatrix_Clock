57eb7ba38a189c41263298c78d5ec007c6225ff3
riaosan <fiatpanda750@icloud.com>
2022-01-28 23:44:53 +0900

    11:00->10:00

--- src/main.cpp
+++ src/main.cpp
@@ -23,15 +23,10 @@ SOFTWARE.
 
 */
 
-#define TS_ENABLE_SSL  // Don't forget it for ThingSpeak.h!!
-
-#include <esp32-hal-log.h>
 #define USE_EFONT
-
+#include <esp32-hal-log.h>
 #include <HD_0158_RG0019A.h>
 #include <AutoConnect.h>
-#include <ESPUI.h>
-#include <ThingSpeak.h>
 #include <Ticker.h>
 #include <WebServer.h>
 #include <WiFi.h>
@@ -44,8 +39,8 @@ SOFTWARE.
 #define HTTP_PORT     80
 #define MSG_CONNECTED "WiFi Started."
 
-#define CLOCK_EN_S    6   //Start AM 6:00
-#define CLOCK_EN_E    23  //End   PM11:00
+#define CLOCK_EN_S    6   // Start AM 6:00
+#define CLOCK_EN_E    22  // End   PM10:00
 
 Ticker clocker;
 Ticker connectBlinker;
@@ -87,92 +82,92 @@ unsigned long myChannelNumber = SECRET_CH_ID;
 const char *myWriteAPIKey     = SECRET_WRITE_APIKEY;
 const char *certificate       = SECRET_TS_ROOT_CA;
 
-//message ID
+// message ID
 enum class MESSAGE {
-    MSG_COMMAND_NOTHING,
-    MSG_COMMAND_GET_SENSOR_DATA,
-    MSG_COMMAND_PRINT_TEMPERATURE_VALUE,
-    MSG_COMMAND_PRINT_PRESSURE_VALUE,
-    MSG_COMMAND_PRINT_HUMIDITY_VALUE,
-    MSG_COMMAND_START_CLOCK,
-    MSG_COMMAND_STOP_CLOCK,
-    MSG_COMMAND_BLE_INIT,
-    MSG_COMMAND_BLE_DO_CONNECT,
-    MSG_COMMAND_BLE_CONNECTED,
-    MSG_COMMAND_BLE_DISCONNECTED,
-    MSG_COMMAND_BLE_NOT_FOUND,
+  MSG_COMMAND_NOTHING,
+  MSG_COMMAND_GET_SENSOR_DATA,
+  MSG_COMMAND_PRINT_TEMPERATURE_VALUE,
+  MSG_COMMAND_PRINT_PRESSURE_VALUE,
+  MSG_COMMAND_PRINT_HUMIDITY_VALUE,
+  MSG_COMMAND_START_CLOCK,
+  MSG_COMMAND_STOP_CLOCK,
+  MSG_COMMAND_BLE_INIT,
+  MSG_COMMAND_BLE_DO_CONNECT,
+  MSG_COMMAND_BLE_CONNECTED,
+  MSG_COMMAND_BLE_DISCONNECTED,
+  MSG_COMMAND_BLE_NOT_FOUND,
 };
 
 MESSAGE message = MESSAGE::MSG_COMMAND_NOTHING;
 
 void setAllPortOutput() {
-    pinMode(PORT_SE_IN, OUTPUT);
-    pinMode(PORT_AB_IN, OUTPUT);
-    pinMode(PORT_LAMP, OUTPUT);
+  pinMode(PORT_SE_IN, OUTPUT);
+  pinMode(PORT_AB_IN, OUTPUT);
+  pinMode(PORT_LAMP, OUTPUT);
 }
 
 void setAllPortLow() {
-    digitalWrite(PORT_SE_IN, LOW);
-    digitalWrite(PORT_AB_IN, LOW);
-    digitalWrite(PORT_LAMP, LOW);
+  digitalWrite(PORT_SE_IN, LOW);
+  digitalWrite(PORT_AB_IN, LOW);
+  digitalWrite(PORT_LAMP, LOW);
 }
 
 void rootPage(void) {
-    String content =
-        "<html>"
-        "<head>"
-        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
-        "<script type=\"text/javascript\">"
-        "setTimeout(\"location.reload()\", 1000);"
-        "</script>"
-        "</head>"
-        "<body>"
-        "<h2 align=\"center\" style=\"color:blue;margin:20px;\">Hello, world</h2>"
-        "<h3 align=\"center\" style=\"color:gray;margin:10px;\">{{DateTime}}</h3>"
-        "<p style=\"text-align:center;\">Reload the page to update the time.</p>"
-        "<p></p><p style=\"padding-top:15px;text-align:center\">" AUTOCONNECT_LINK(COG_24) "</p>"
-                                                                                           "</body>"
-                                                                                           "</html>";
-    static const char *wd[7] = {"Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"};
-    struct tm *tm;
-    time_t t;
-    char dateTime[26];
+  String content =
+      "<html>"
+      "<head>"
+      "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+      "<script type=\"text/javascript\">"
+      "setTimeout(\"location.reload()\", 1000);"
+      "</script>"
+      "</head>"
+      "<body>"
+      "<h2 align=\"center\" style=\"color:blue;margin:20px;\">Hello, world</h2>"
+      "<h3 align=\"center\" style=\"color:gray;margin:10px;\">{{DateTime}}</h3>"
+      "<p style=\"text-align:center;\">Reload the page to update the time.</p>"
+      "<p></p><p style=\"padding-top:15px;text-align:center\">" AUTOCONNECT_LINK(COG_24) "</p>"
+                                                                                         "</body>"
+                                                                                         "</html>";
+  static const char *wd[7] = {"Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"};
+  struct tm *tm;
+  time_t t;
+  char dateTime[26];
 
-    t  = time(NULL);
-    tm = localtime(&t);
-    sprintf(dateTime, "%04d/%02d/%02d(%s) %02d:%02d:%02d.",
-            tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
-            wd[tm->tm_wday],
-            tm->tm_hour, tm->tm_min, tm->tm_sec);
-    content.replace("{{DateTime}}", String(dateTime));
-    Server.send(200, "text/html", content);
+  t  = time(NULL);
+  tm = localtime(&t);
+  sprintf(dateTime, "%04d/%02d/%02d(%s) %02d:%02d:%02d.",
+          tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
+          wd[tm->tm_wday],
+          tm->tm_hour, tm->tm_min, tm->tm_sec);
+  content.replace("{{DateTime}}", String(dateTime));
+  Server.send(200, "text/html", content);
 }
 
 void startPage(void) {
-    // Retrieve the value of AutoConnectElement with arg function of WebServer class.
-    // Values are accessible with the element name.
-    String tz = Server.arg("timezone");
+  // Retrieve the value of AutoConnectElement with arg function of WebServer class.
+  // Values are accessible with the element name.
+  String tz = Server.arg("timezone");
 
-    for (uint8_t n = 0; n < sizeof(TZ) / sizeof(Timezone_t); n++) {
-        String tzName = String(TZ[n].zone);
-        if (tz.equalsIgnoreCase(tzName)) {
-            configTime(TZ[n].tzoff * 3600, 0, TZ[n].ntpServer);
-            log_d("Time zone: %s", tz.c_str());
-            log_d("ntp server: %s", String(TZ[n].ntpServer).c_str());
-            break;
-        }
+  for (uint8_t n = 0; n < sizeof(TZ) / sizeof(Timezone_t); n++) {
+    String tzName = String(TZ[n].zone);
+    if (tz.equalsIgnoreCase(tzName)) {
+      configTime(TZ[n].tzoff * 3600, 0, TZ[n].ntpServer);
+      log_d("Time zone: %s", tz.c_str());
+      log_d("ntp server: %s", String(TZ[n].ntpServer).c_str());
+      break;
     }
+  }
 
-    // The /start page just constitutes timezone,
-    // it redirects to the root page without the content response.
-    Server.sendHeader("Location", String("http://") + Server.client().localIP().toString() + String("/"));
-    Server.send(302, "text/plain", "");
-    Server.client().flush();
-    Server.client().stop();
+  // The /start page just constitutes timezone,
+  // it redirects to the root page without the content response.
+  Server.sendHeader("Location", String("http://") + Server.client().localIP().toString() + String("/"));
+  Server.send(302, "text/plain", "");
+  Server.client().flush();
+  Server.client().stop();
 }
 
 void otaPage(void) {
-    String content = R"(
+  String content = R"(
         <!DOCTYPE html>
         <html>
         <head>
@@ -185,247 +180,219 @@ void otaPage(void) {
         </html>
     )";
 
-    content.replace("__AC_LINK__", String(AUTOCONNECT_LINK(COG_16)));
-    Server.send(200, "text/html", content);
+  content.replace("__AC_LINK__", String(AUTOCONNECT_LINK(COG_16)));
+  Server.send(200, "text/html", content);
 }
 
 String makeCreateTime() {
-    time_t t      = time(NULL);
-    struct tm *tm = localtime(&t);
+  time_t t      = time(NULL);
+  struct tm *tm = localtime(&t);
 
-    char buffer[128] = {0};
-    sprintf(buffer, "%04d-%02d-%02dT%02d:%02d:%02d+0900",
-            tm->tm_year + 1900,
-            tm->tm_mon + 1,
-            tm->tm_mday,
-            tm->tm_hour,
-            tm->tm_min,
-            tm->tm_sec);
+  char buffer[128] = {0};
+  sprintf(buffer, "%04d-%02d-%02dT%02d:%02d:%02d+0900",
+          tm->tm_year + 1900,
+          tm->tm_mon + 1,
+          tm->tm_mday,
+          tm->tm_hour,
+          tm->tm_min,
+          tm->tm_sec);
 
-    log_i("[time] %s", String(buffer).c_str());
+  log_i("[time] %s", String(buffer).c_str());
 
-    return String(buffer);
+  return String(buffer);
 }
 
 void printTimeLEDMatrix(void) {
-    static int flag = 0;
-    flag            = ~flag;
+  static int flag = 0;
+  flag            = ~flag;
 
-    char tmp_str[10] = {0};
-    time_t t         = time(NULL);
-    struct tm *tm    = localtime(&t);
+  char tmp_str[10] = {0};
+  time_t t         = time(NULL);
+  struct tm *tm    = localtime(&t);
 
-    if (flag == 0) {
-        sprintf(tmp_str, "%02d:%02d:%02d", tm->tm_hour, tm->tm_min, tm->tm_sec);
-    } else {
-        sprintf(tmp_str, "%02d:%02d:%02d", tm->tm_hour, tm->tm_min, tm->tm_sec);
-    }
+  if (flag == 0) {
+    sprintf(tmp_str, "%02d:%02d:%02d", tm->tm_hour, tm->tm_min, tm->tm_sec);
+  } else {
+    sprintf(tmp_str, "%02d:%02d:%02d", tm->tm_hour, tm->tm_min, tm->tm_sec);
+  }
 
-    matrix.startWrite();
-    matrix.setCursor(0, -1);
-    matrix.setTextColor(DOT_GREEN, DOT_BLACK);
-    matrix.printEfont(tmp_str);
-    matrix.endWrite();
+  matrix.startWrite();
+  matrix.setCursor(0, -1);
+  matrix.setTextColor(DOT_GREEN, DOT_BLACK);
+  matrix.printEfont(tmp_str);
+  matrix.endWrite();
 }
 
 void connecting() {
-    static int num = 0;
+  static int num = 0;
 
-    num = ~num;
+  num = ~num;
 
-    matrix.startWrite();
-    matrix.fillScreen(DOT_BLACK);
-    matrix.setCursor(0, 0);
-    matrix.setTextColor(DOT_GREEN, DOT_BLACK);
+  matrix.startWrite();
+  matrix.fillScreen(DOT_BLACK);
+  matrix.setCursor(0, 0);
+  matrix.setTextColor(DOT_GREEN, DOT_BLACK);
 
-    if (num) {
-        matrix.printEfont("init");
-        matrix.setTextColor(DOT_ORANGE, DOT_BLACK);
-        matrix.printEfont(".");
-    } else {
-        matrix.printEfont("init");
-    }
+  if (num) {
+    matrix.printEfont("init");
+    matrix.setTextColor(DOT_ORANGE, DOT_BLACK);
+    matrix.printEfont(".");
+  } else {
+    matrix.printEfont("init");
+  }
 
-    matrix.endWrite();
+  matrix.endWrite();
 }
 
 void initMatrix(void) {
-    setAllPortOutput();
-    setAllPortLow();
+  setAllPortOutput();
+  setAllPortLow();
 
-    matrix.begin();
+  matrix.begin();
 #ifdef DEBUG
-    delay(1000);
-    matrix.fillScreen(DOT_GREEN);
-    delay(1000);
-    matrix.fillScreen(DOT_RED);
-    delay(1000);
+  delay(1000);
+  matrix.fillScreen(DOT_GREEN);
+  delay(1000);
+  matrix.fillScreen(DOT_RED);
+  delay(1000);
 #endif
-    matrix.fillScreen(DOT_BLACK);
+  matrix.fillScreen(DOT_BLACK);
 
-    matrix.setTextWrap(false);
-    matrix.setTextSize(1);  // x1
-    matrix.setCursor(0, 0);
-    matrix.setRotation(0);
+  matrix.setTextWrap(false);
+  matrix.setTextSize(1);  // x1
+  matrix.setCursor(0, 0);
+  matrix.setRotation(0);
 }
 
 bool check_clock_enable(uint8_t start_hour, uint8_t end_hour) {
-    time_t t      = time(NULL);
-    struct tm *tm = localtime(&t);
+  time_t t      = time(NULL);
+  struct tm *tm = localtime(&t);
 
-    log_i("HH:MM:SS = %02d:%02d:%02d", tm->tm_hour, tm->tm_min, tm->tm_sec);
+  log_i("HH:MM:SS = %02d:%02d:%02d", tm->tm_hour, tm->tm_min, tm->tm_sec);
 
-    if (start_hour <= tm->tm_hour && tm->tm_hour < end_hour) {
-        return true;
-    } else {
-        return false;
-    }
+  if (start_hour <= tm->tm_hour && tm->tm_hour < end_hour) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 void checkSensor() {
-    message = MESSAGE::MSG_COMMAND_GET_SENSOR_DATA;
+  message = MESSAGE::MSG_COMMAND_GET_SENSOR_DATA;
 }
 
 void stopClock() {
-    clocker.detach();
+  clocker.detach();
 }
 
 void startClock() {
-    clocker.attach_ms(250, printTimeLEDMatrix);
+  clocker.attach_ms(250, printTimeLEDMatrix);
 }
 
 void check_clock() {
-    if (check_clock_enable(CLOCK_EN_S, CLOCK_EN_E)) {
-        message = MESSAGE::MSG_COMMAND_START_CLOCK;
-    } else {
-        stopClock();
-        matrix.fillScreen(DOT_BLACK);
-        digitalWrite(PORT_LAMP, LOW);
-    }
+  if (check_clock_enable(CLOCK_EN_S, CLOCK_EN_E)) {
+    message = MESSAGE::MSG_COMMAND_START_CLOCK;
+  } else {
+    stopClock();
+    matrix.fillScreen(DOT_BLACK);
+    digitalWrite(PORT_LAMP, LOW);
+  }
 }
 
 bool WaitSeconds(int second) {
-    time_t t      = time(NULL);
-    struct tm *tm = localtime(&t);
+  time_t t      = time(NULL);
+  struct tm *tm = localtime(&t);
 
-    if (tm->tm_sec == second) {
-        return false;
-    }
+  if (tm->tm_sec == second) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 void initClock() {
-    //Get NTP Time
-    configTzTime("JST-9", "ntp.nict.jp", "ntp.jst.mfeed.ad.jp");
-    delay(2000);
+  // Get NTP Time
+  configTzTime("JST-9", "ntp.nict.jp", "ntp.jst.mfeed.ad.jp");
+  delay(2000);
 
-    while (WaitSeconds(0)) {
-        delay(100);
-        yield();
-    }
+  while (WaitSeconds(0)) {
+    delay(100);
+    yield();
+  }
 
-    check_clock();
-    clockChecker.attach(60, check_clock);
+  check_clock();
+  clockChecker.attach(60, check_clock);
 
-    while (WaitSeconds(30)) {
-        delay(100);
-        yield();
-    }
-}
-
-void selectHour(Control *sender, int value) {
-    log_d("Select: ID: %d, Value: %d", sender->id, sender->value);
-}
-
-void selectMinuit(Control *sender, int value) {
-    log_d("Select: ID: %d, Value: %d", sender->id, sender->value);
-}
-
-void initESPUI() {
-    ESPUI.setVerbosity(Verbosity::Quiet);
-
-    uint16_t hour = ESPUI.addControl(ControlType::Select, "Hour", "", ControlColor::Alizarin, Control::noParent, &selectHour);
-    ESPUI.addControl(ControlType::Option, "6 am", "6", ControlColor::Alizarin, hour);
-    ESPUI.addControl(ControlType::Option, "7 am", "7", ControlColor::Alizarin, hour);
-    ESPUI.addControl(ControlType::Option, "8 am", "8", ControlColor::Alizarin, hour);
-
-    uint16_t minuit = ESPUI.addControl(ControlType::Select, "Minuit", "", ControlColor::Alizarin, Control::noParent, &selectMinuit);
-    ESPUI.addControl(ControlType::Option, "0", "0", ControlColor::Alizarin, minuit);
-    ESPUI.addControl(ControlType::Option, "10", "10", ControlColor::Alizarin, minuit);
-    ESPUI.addControl(ControlType::Option, "20", "20", ControlColor::Alizarin, minuit);
-    ESPUI.addControl(ControlType::Option, "30", "30", ControlColor::Alizarin, minuit);
-    ESPUI.addControl(ControlType::Option, "40", "40", ControlColor::Alizarin, minuit);
-    ESPUI.addControl(ControlType::Option, "50", "50", ControlColor::Alizarin, minuit);
-
-    ESPUI.begin("LAMP Alarm Clock Setting");
+  while (WaitSeconds(30)) {
+    delay(100);
+    yield();
+  }
 }
 
 void initAutoConnect(void) {
-    Serial.begin(115200);
-    // Enable saved past credential by autoReconnect option,
-    // even once it is disconnected.
-    Config.autoReconnect = true;
-    Config.ota           = AC_OTA_BUILTIN;
-    Portal.config(Config);
+  Serial.begin(115200);
+  // Enable saved past credential by autoReconnect option,
+  // even once it is disconnected.
+  Config.autoReconnect = true;
+  Config.ota           = AC_OTA_BUILTIN;
+  Portal.config(Config);
 
-    // Load aux. page
-    Timezone.load(AUX_TIMEZONE);
-    // Retrieve the select element that holds the time zone code and
-    // register the zone mnemonic in advance.
-    AutoConnectSelect &tz = Timezone["timezone"].as<AutoConnectSelect>();
-    for (uint8_t n = 0; n < sizeof(TZ) / sizeof(Timezone_t); n++) {
-        tz.add(String(TZ[n].zone));
-    }
+  // Load aux. page
+  Timezone.load(AUX_TIMEZONE);
+  // Retrieve the select element that holds the time zone code and
+  // register the zone mnemonic in advance.
+  AutoConnectSelect &tz = Timezone["timezone"].as<AutoConnectSelect>();
+  for (uint8_t n = 0; n < sizeof(TZ) / sizeof(Timezone_t); n++) {
+    tz.add(String(TZ[n].zone));
+  }
 
-    Portal.join({Timezone});  // Register aux. page
+  Portal.join({Timezone});  // Register aux. page
 
-    // Behavior a root path of ESP8266WebServer.
-    Server.on("/", rootPage);
-    Server.on("/start", startPage);  // Set NTP server trigger handler
-    Server.on("/ota", otaPage);
+  // Behavior a root path of ESP8266WebServer.
+  Server.on("/", rootPage);
+  Server.on("/start", startPage);  // Set NTP server trigger handler
+  Server.on("/ota", otaPage);
 
-    // Establish a connection with an autoReconnect option.
-    if (Portal.begin()) {
-        log_i("WiFi connected: %s", WiFi.localIP().toString().c_str());
-        if (MDNS.begin(HOSTNAME)) {
-            MDNS.addService("http", "tcp", HTTP_PORT);
-            log_i("HTTP Server ready! Open http://%s.local/ in your browser\n", HOSTNAME);
-        } else
-            log_e("Error setting up MDNS responder");
-    }
+  // Establish a connection with an autoReconnect option.
+  if (Portal.begin()) {
+    log_i("WiFi connected: %s", WiFi.localIP().toString().c_str());
+    if (MDNS.begin(HOSTNAME)) {
+      MDNS.addService("http", "tcp", HTTP_PORT);
+      log_i("HTTP Server ready! Open http://%s.local/ in your browser\n", HOSTNAME);
+    } else
+      log_e("Error setting up MDNS responder");
+  }
 }
 
 void setup() {
-    initMatrix();
+  initMatrix();
 
-    connectBlinker.attach_ms(500, connecting);
+  connectBlinker.attach_ms(500, connecting);
 
-    initAutoConnect();
-    initClock();
-    //initESPUI();
+  initAutoConnect();
+  initClock();
 
-    connectBlinker.detach();
+  connectBlinker.detach();
 }
 
 void loop() {
-    Portal.handleClient();
-    switch (message) {
-        case MESSAGE::MSG_COMMAND_GET_SENSOR_DATA:
-            message = MESSAGE::MSG_COMMAND_NOTHING;
-            break;
-        case MESSAGE::MSG_COMMAND_START_CLOCK:
-            digitalWrite(PORT_LAMP, HIGH);
-            startClock();
-            message = MESSAGE::MSG_COMMAND_NOTHING;
-            break;
-        case MESSAGE::MSG_COMMAND_STOP_CLOCK:
-            stopClock();
-            message = MESSAGE::MSG_COMMAND_PRINT_TEMPERATURE_VALUE;
-            break;
-        case MESSAGE::MSG_COMMAND_NOTHING:
-        default:;  //nothing
-    }
+  Portal.handleClient();
+  switch (message) {
+    case MESSAGE::MSG_COMMAND_GET_SENSOR_DATA:
+      message = MESSAGE::MSG_COMMAND_NOTHING;
+      break;
+    case MESSAGE::MSG_COMMAND_START_CLOCK:
+      digitalWrite(PORT_LAMP, HIGH);
+      startClock();
+      message = MESSAGE::MSG_COMMAND_NOTHING;
+      break;
+    case MESSAGE::MSG_COMMAND_STOP_CLOCK:
+      stopClock();
+      message = MESSAGE::MSG_COMMAND_PRINT_TEMPERATURE_VALUE;
+      break;
+    case MESSAGE::MSG_COMMAND_NOTHING:
+    default:;  // nothing
+  }
 
-    yield();
+  delay(1);
 }
